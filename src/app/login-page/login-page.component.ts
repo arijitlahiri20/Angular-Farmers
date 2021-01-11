067f085f12545cf51f8720a92de04653d021ce40
pannagahnagraj <pannaga.hn@lntinfotech.com>
2021-01-11 12:50:40 +0530

    login,signup,farmer registration,model added

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../models/login.model';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  login: Login = new Login();
+  message: string;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  // loginCheck() {
+  //   console.log(this.login);
+  //   this.userService.login(this.login).subscribe(response => {
+  //     alert(JSON.stringify(response));
+  //     console.log(response);
+  //     if(response.status == 'SUCCESS') {
+  //       let userId = response.userId;
+  //       let userName = response.userName;
+  //       sessionStorage.setItem('userId', String(userId));
+  //       sessionStorage.setItem('userName', userName);
+  //       this.router.navigate(['farmer-home']);
+  //     }
+  //     else
+  //       this.message = response.message;
+  //   })
+  // }
+  
 }
