11118e52c6d568dd6c8b22ede20b221da472ccaa
pannagahnagraj <pannaga.hn@lntinfotech.com>
2021-01-15 17:06:22 +0530

    bidder module modified

--- src/app/bidder-marketplace/bidder-marketplace.component.ts
+++ src/app/bidder-marketplace/bidder-marketplace.component.ts
@@ -11,6 +11,7 @@ import { BidderService } from '../services/bidder.service';
 export class BidderMarketplaceComponent implements OnInit {
 
   Sellrequests: any=[];
+  sell_id :any;
   constructor(private service:BidderService, private router:Router) { }
 
   ngOnInit() {
@@ -22,9 +23,8 @@ export class BidderMarketplaceComponent implements OnInit {
     this.service.biddermarket().subscribe(data => {
       console.log(JSON.stringify(data));
       this.Sellrequests=data.list;
-
-
     })
+
   }
 
   placebid(sell_id){
--- src/app/services/bidder.service.ts
+++ src/app/services/bidder.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Bids } from '../models/bids.model';
+import { Sellrequests } from '../models/sellrequests.model';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -26,6 +27,11 @@ export class BidderService {
   biddermarket() : Observable<any> {
     let url = this.baseURL + "/bidder/marketplace";
    return this.http.get(url); 
+  }
+
+  getTopBid(sell) : Observable<any> {
+    let url = this.baseURL + "/bidder/topbid";
+   return this.http.post(url, sell); 
   }
 
 }
