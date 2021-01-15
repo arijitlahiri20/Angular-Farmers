47030d05b204d41d5aa39de863fc7d446ea452cd
sneha <sneha.ukarande@lntinfotech.com>
2021-01-15 17:43:34 +0530

    insuranceClaimFuctionalities

--- src/app/insurance-claim/insurance-claim.component.ts
+++ src/app/insurance-claim/insurance-claim.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+//import { Insurance } from '../models/insurance.model';
+import { InsuranceService } from '../services/insurance.service';
+import { Router } from '@angular/router';
+import { Claims } from '../models/claims.model';
 
 @Component({
   selector: 'app-insurance-claim',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsuranceClaimComponent implements OnInit {
 
-  constructor() { }
+  claims =new Claims();
+
+  constructor(private service:InsuranceService,private router: Router) { }
 
   ngOnInit() {
   }
+
+  InsuranceClaimCheck(){
+
+    this.claims.insurance_id= this.claims.policy_no;
+    this.claims.status="PENDING";
+  
+   console.log(this.claims);
+
+   this.service.signupclaim(this.claims).subscribe(data=>{
+        
+        //this.message=data.message;
+        alert(JSON.stringify(data));
+        if(data.status=="SUCCESS")
+          this.router.navigate(['/insurance-homeApplyInsurance()']);
+   })
+}
 
 }
---
+++ src/app/models/insurance.model.ts
@@ -0,0 +1,13 @@
+export class Insurance {
+    insurance_id: number;
+    user_id: number;
+    season:string;
+    year :number;
+    crop :string;
+    sum_insured:number;
+    area : number;
+    premium_amount:number;
+    insurance_company: string;
+    policy_no: string;
+    status :string;
+}
